37469c61c89eac589a7a5c213e94f450d7a3bab8
Max Kowarski <starinfidel@gmail.com>
2026-02-26 01:37:16 +0500

    weighted wall decoration, more layers

--- src/tiles/Tileset.hx
+++ src/tiles/Tileset.hx
@@ -3,6 +3,7 @@
 package tiles;
 
 import Const;
+import game.AreaGame;
 import js.html.CanvasRenderingContext2D;
 import js.html.Image;
 
@@ -11,6 +12,7 @@ class Tileset
   public var image: Image;
   public var voidTile: _Icon;
   public var wallDecorationLayers: Array<Image>;
+  public var wallDecorationLayerWeights: Array<Int>;
 
 // create tileset from image path
   public function new(path: String)
@@ -22,6 +24,7 @@ class Tileset
         col: 0,
       };
       wallDecorationLayers = [];
+      wallDecorationLayerWeights = [];
     }
 
 // map tile id to icon coordinates
@@ -48,11 +51,12 @@ class Tileset
     }
 
 // add one wall decoration image layer
-  public function addWallDecorationLayer(path: String)
+  public function addWallDecorationLayer(path: String, weight: Int = 100)
     {
       var layer = new Image();
       layer.src = path;
       wallDecorationLayers.push(layer);
+      wallDecorationLayerWeights.push(weight);
     }
 
 // get number of wall decoration image layers
@@ -65,6 +69,54 @@ class Tileset
   public function isWallTile(tileID: Int): Bool
     {
       return false;
+    }
+
+// get percent chance to place wall decoration on wall tile
+  public function getWallDecorationChance(): Int
+    {
+      return 60;
+    }
+
+// place one wall decoration descriptor on tile if random checks pass
+  public function decorateWallTile(area: AreaGame, x: Int, y: Int)
+    {
+      if (wallDecorationLayers.length <= 0)
+        return;
+
+      var tileID = area.getCellType(x, y);
+      if (!isWallTile(tileID) ||
+          Std.random(100) >= getWallDecorationChance())
+        return;
+
+      var layerID = pickWallDecorationLayerID();
+      if (layerID < 0)
+        return;
+      area.addTileDecoration(x, y, {
+        layerID: layerID,
+      });
+    }
+
+// pick one wall decoration layer index by configured weights
+  function pickWallDecorationLayerID(): Int
+    {
+      var totalWeight = 0;
+      for (layerWeight in wallDecorationLayerWeights)
+        if (layerWeight > 0)
+          totalWeight += layerWeight;
+      if (totalWeight <= 0)
+        return -1;
+
+      var roll = Std.random(totalWeight);
+      for (i in 0...wallDecorationLayerWeights.length)
+        {
+          var layerWeight = wallDecorationLayerWeights[i];
+          if (layerWeight <= 0)
+            continue;
+          if (roll < layerWeight)
+            return i;
+          roll -= layerWeight;
+        }
+      return -1;
     }
 
 // draw one tile icon at screen position
