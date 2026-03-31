48a073f4028a3518c0c7491fc73c970b25131f54
Max Kowarski <starinfidel@gmail.com>
2026-03-31 22:35:01 +0400

    Split map building pipeline helpers

---
+++ src/map/buildings/BuildingsPainter.hx
@@ -0,0 +1,288 @@
+// building paint helpers
+
+package map.buildings;
+
+import map.Buildings;
+import map.Types.BuildingFootprint;
+import map.Types.ParcelRect;
+
+@:access(map.Core)
+@:access(map.Ground)
+@:access(map.Raster)
+@:access(map.RoadPlan)
+@:access(map.LegacyRoads)
+@:access(map.Buildings)
+class BuildingsPainter
+{
+  var plan: Buildings;
+
+  public function new(plan: Buildings)
+    {
+      this.plan = plan;
+    }
+
+// paint open parcels as yards or plazas
+  public function paintOpenParcels(parcelList: Array<ParcelRect>)
+    {
+      for (parcel in parcelList)
+        {
+          if (!parcel.isOpen)
+            continue;
+
+          var sizingDensity = plan.getBuildingDistrictDensity(parcel.districtType);
+          var inset = plan.PLAN_CELL_SIZE;
+          if (sizingDensity < 0.33)
+            inset = plan.PLAN_CELL_SIZE + 2;
+          else if (sizingDensity > 0.66)
+            inset = plan.PLAN_CELL_SIZE;
+
+          var x = parcel.x + inset;
+          var y = parcel.y + inset;
+          var width = parcel.width - inset * 2;
+          var height = parcel.height - inset * 2;
+          if (width < plan.PLAN_CELL_SIZE ||
+              height < plan.PLAN_CELL_SIZE)
+            continue;
+
+          if (sizingDensity > 0.66)
+            paintPlaza(x, y, width, height, parcel);
+          else
+            paintYard(x, y, width, height, parcel);
+        }
+    }
+
+// paint all building footprints
+  public function paintBuildings()
+    {
+      for (building in plan.buildings)
+        paintBuilding(building);
+    }
+
+// paint one building footprint with a soft edge and shadow
+  function paintBuilding(building: BuildingFootprint)
+    {
+      var rooftopRectCount = plan.getBuildingRooftopRectCount(building);
+      var baseRectCount = plan.getBuildingBaseRectCount(building);
+      var rooftopColor = plan.getBuildingPaintRooftopColor(building);
+      var shadowColor = plan.adjustColor(plan.COLOR_BUILDING_SHADOW,
+        0.96 + building.density * 0.08);
+      var highlightColor = plan.adjustColor(building.roofColor, 1.07);
+      var shadeColor = plan.adjustColor(building.color, 0.76);
+      var rooftopRoofColor = plan.adjustColor(building.roofColor, 1.04);
+      var rooftopHighlightColor = plan.adjustColor(building.roofColor, 1.10);
+      var rooftopShadeColor = plan.adjustColor(rooftopColor, 0.80);
+
+      if (building.forecourtAlpha > 0.0)
+        paintBuildingForecourt(building);
+
+      plan.ctx.fillStyle = '#' + StringTools.hex(building.color, 6);
+      plan.ctx.globalAlpha = building.edgeAlpha;
+      for (i in 0...baseRectCount)
+        {
+          var rect = building.rects[i];
+          if (plan.isSmallBuildingRect(rect))
+            continue;
+          plan.ctx.fillRect(rect.x - 2, rect.y - 2, rect.width + 4, rect.height + 4);
+        }
+      if (rooftopRectCount > 0)
+        {
+          plan.ctx.fillStyle = '#' + StringTools.hex(rooftopColor, 6);
+          for (i in baseRectCount...building.rects.length)
+            {
+              var rect = building.rects[i];
+              if (plan.isSmallBuildingRect(rect))
+                continue;
+              plan.ctx.fillRect(rect.x - 2, rect.y - 2, rect.width + 4, rect.height + 4);
+            }
+        }
+
+      plan.ctx.fillStyle = '#' + StringTools.hex(shadowColor, 6);
+      plan.ctx.globalAlpha = building.shadowAlpha;
+      for (i in 0...baseRectCount)
+        {
+          var rect = building.rects[i];
+          if (plan.isSmallBuildingRect(rect))
+            continue;
+          plan.ctx.fillRect(rect.x + building.shadowOffset, rect.y + building.shadowOffset,
+            rect.width, rect.height);
+        }
+      if (rooftopRectCount > 0)
+        {
+          for (i in baseRectCount...building.rects.length)
+            {
+              var rect = building.rects[i];
+              if (plan.isSmallBuildingRect(rect))
+                continue;
+              plan.ctx.fillRect(rect.x + building.shadowOffset, rect.y + building.shadowOffset,
+                rect.width, rect.height);
+            }
+        }
+
+      plan.ctx.fillStyle = '#' + StringTools.hex(building.color, 6);
+      plan.ctx.globalAlpha = 1.0;
+      for (i in 0...baseRectCount)
+        {
+          var rect = building.rects[i];
+          plan.ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+        }
+      if (rooftopRectCount > 0)
+        {
+          plan.ctx.fillStyle = '#' + StringTools.hex(rooftopColor, 6);
+          for (i in baseRectCount...building.rects.length)
+            {
+              var rect = building.rects[i];
+              plan.ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+            }
+        }
+
+      plan.ctx.fillStyle = '#' + StringTools.hex(building.roofColor, 6);
+      plan.ctx.globalAlpha = building.roofAlpha;
+      for (i in 0...baseRectCount)
+        {
+          var rect = building.rects[i];
+          if (plan.isSmallBuildingRect(rect))
+            continue;
+          var roofInset = plan.getBuildingRoofInset(rect, building.density);
+          var roofWidth = rect.width - roofInset * 2;
+          var roofHeight = rect.height - roofInset * 2;
+          if (roofWidth < plan.CLEAN_TILE_SIZE / 5 ||
+              roofHeight < plan.CLEAN_TILE_SIZE / 5)
+            continue;
+
+          plan.ctx.fillRect(rect.x + roofInset, rect.y + roofInset, roofWidth, roofHeight);
+        }
+      if (rooftopRectCount > 0)
+        {
+          plan.ctx.fillStyle = '#' + StringTools.hex(rooftopRoofColor, 6);
+          for (i in baseRectCount...building.rects.length)
+            {
+              var rect = building.rects[i];
+              if (plan.isSmallBuildingRect(rect))
+                continue;
+              var roofInset = plan.getBuildingRoofInset(rect, building.density);
+              var roofWidth = rect.width - roofInset * 2;
+              var roofHeight = rect.height - roofInset * 2;
+              if (roofWidth < plan.CLEAN_TILE_SIZE / 5 ||
+                  roofHeight < plan.CLEAN_TILE_SIZE / 5)
+                continue;
+
+              plan.ctx.fillRect(rect.x + roofInset, rect.y + roofInset, roofWidth, roofHeight);
+            }
+        }
+
+      plan.ctx.fillStyle = '#' + StringTools.hex(highlightColor, 6);
+      plan.ctx.globalAlpha = 0.08 + building.edgeAlpha * 0.45;
+      for (i in 0...baseRectCount)
+        {
+          var rect = building.rects[i];
+          if (plan.isSmallBuildingRect(rect))
+            continue;
+          var edgeWidth = plan.getBuildingEdgeWidth(rect, building.density);
+          plan.ctx.fillRect(rect.x, rect.y, rect.width, edgeWidth);
+          plan.ctx.fillRect(rect.x, rect.y, edgeWidth, rect.height);
+        }
+      if (rooftopRectCount > 0)
+        {
+          plan.ctx.fillStyle = '#' + StringTools.hex(rooftopHighlightColor, 6);
+          for (i in baseRectCount...building.rects.length)
+            {
+              var rect = building.rects[i];
+              if (plan.isSmallBuildingRect(rect))
+                continue;
+              var edgeWidth = plan.getBuildingEdgeWidth(rect, building.density);
+              plan.ctx.fillRect(rect.x, rect.y, rect.width, edgeWidth);
+              plan.ctx.fillRect(rect.x, rect.y, edgeWidth, rect.height);
+            }
+        }
+
+      plan.ctx.fillStyle = '#' + StringTools.hex(shadeColor, 6);
+      plan.ctx.globalAlpha = 0.11 + building.edgeAlpha * 0.55;
+      for (i in 0...baseRectCount)
+        {
+          var rect = building.rects[i];
+          if (plan.isSmallBuildingRect(rect))
+            continue;
+          var edgeWidth = plan.getBuildingEdgeWidth(rect, building.density);
+          plan.ctx.fillRect(rect.x, rect.y + rect.height - edgeWidth, rect.width, edgeWidth);
+          plan.ctx.fillRect(rect.x + rect.width - edgeWidth, rect.y, edgeWidth, rect.height);
+        }
+      if (rooftopRectCount > 0)
+        {
+          plan.ctx.fillStyle = '#' + StringTools.hex(rooftopShadeColor, 6);
+          for (i in baseRectCount...building.rects.length)
+            {
+              var rect = building.rects[i];
+              if (plan.isSmallBuildingRect(rect))
+                continue;
+              var edgeWidth = plan.getBuildingEdgeWidth(rect, building.density);
+              plan.ctx.fillRect(rect.x, rect.y + rect.height - edgeWidth, rect.width, edgeWidth);
+              plan.ctx.fillRect(rect.x + rect.width - edgeWidth, rect.y, edgeWidth,
+                rect.height);
+            }
+        }
+
+      plan.ctx.globalAlpha = 1.0;
+    }
+
+// paint a paved forecourt around a centered tower footprint
+  function paintBuildingForecourt(building: BuildingFootprint)
+    {
+      var inset = 3;
+      var x = building.lotX + inset;
+      var y = building.lotY + inset;
+      var width = building.lotWidth - inset * 2;
+      var height = building.lotHeight - inset * 2;
+      if (width < plan.CLEAN_TILE_SIZE / 2 ||
+          height < plan.CLEAN_TILE_SIZE / 2)
+        return;
+
+      plan.ctx.globalAlpha = building.forecourtAlpha;
+      plan.ctx.fillStyle = '#' + StringTools.hex(building.forecourtColor, 6);
+      plan.ctx.fillRect(x, y, width, height);
+
+      plan.ctx.globalAlpha = building.forecourtAlpha * 0.48;
+      plan.ctx.fillStyle = '#' + StringTools.hex(
+        plan.adjustColor(building.forecourtColor, 1.08), 6);
+      plan.ctx.fillRect(x + Std.int(width / 2) - 2, y + 4, 4, height - 8);
+      plan.ctx.fillRect(x + 4, y + Std.int(height / 2) - 2, width - 8, 4);
+      plan.ctx.globalAlpha = 1.0;
+    }
+
+// paint one high-density open parcel as a plaza
+  function paintPlaza(x: Int, y: Int, width: Int, height: Int, parcel: ParcelRect)
+    {
+      plan.ctx.globalAlpha = 0.46;
+      plan.ctx.fillStyle = '#' + StringTools.hex(plan.adjustColor(plan.COLOR_PLAZA,
+        0.92 + plan.hashFloat(parcel.x, parcel.y, 211) * 0.14), 6);
+      plan.ctx.fillRect(x, y, width, height);
+
+      plan.ctx.globalAlpha = 0.28;
+      plan.ctx.fillStyle = '#' + StringTools.hex(plan.COLOR_PLAZA_EDGE, 6);
+      plan.ctx.fillRect(x + Std.int(width / 2) - 2, y + 4, 4, height - 8);
+      plan.ctx.fillRect(x + 4, y + Std.int(height / 2) - 2, width - 8, 4);
+      if (width > plan.CLEAN_TILE_SIZE * 2 &&
+          height > plan.CLEAN_TILE_SIZE * 2)
+        {
+          plan.ctx.globalAlpha = 0.12;
+          plan.ctx.fillRect(x + Std.int(width / 4) - 1, y + 6, 2, height - 12);
+          plan.ctx.fillRect(x + Std.int(width * 3 / 4) - 1, y + 6, 2, height - 12);
+          plan.ctx.fillRect(x + 6, y + Std.int(height / 4) - 1, width - 12, 2);
+          plan.ctx.fillRect(x + 6, y + Std.int(height * 3 / 4) - 1, width - 12, 2);
+        }
+      plan.ctx.globalAlpha = 1.0;
+    }
+
+// paint one lower-density open parcel as a yard
+  function paintYard(x: Int, y: Int, width: Int, height: Int, parcel: ParcelRect)
+    {
+      plan.ctx.globalAlpha = 0.24;
+      plan.ctx.fillStyle = '#' + StringTools.hex(plan.adjustColor(plan.COLOR_YARD,
+        0.92 + plan.hashFloat(parcel.x, parcel.y, 223) * 0.18), 6);
+      plan.ctx.fillRect(x, y, width, height);
+      plan.ctx.globalAlpha = 0.10;
+      plan.ctx.fillStyle = '#' + StringTools.hex(plan.adjustColor(plan.COLOR_YARD, 1.10), 6);
+      plan.ctx.fillRect(x + 4, y + 4, width - 8, height - 8);
+      plan.ctx.globalAlpha = 1.0;
+    }
+
+}
---
+++ src/map/buildings/BuildingsParcelBuilder.hx
@@ -0,0 +1,171 @@
+// building parcel subdivision helpers
+
+package map.buildings;
+
+import map.Buildings;
+import map.Types.BlockRect;
+import map.Types.ParcelRect;
+
+@:access(map.Core)
+@:access(map.Ground)
+@:access(map.Raster)
+@:access(map.RoadPlan)
+@:access(map.LegacyRoads)
+@:access(map.Buildings)
+class BuildingsParcelBuilder
+{
+  var plan: Buildings;
+
+  public function new(plan: Buildings)
+    {
+      this.plan = plan;
+    }
+
+// subdivide blocks into orthogonal parcels
+  public function buildParcels(blockList: Array<BlockRect>): Array<ParcelRect>
+    {
+      var result = [];
+
+      for (block in blockList)
+        {
+          var setback = plan.getBlockSetback(plan.getBuildingDistrictDensity(block.districtType));
+          var innerX = block.x + setback;
+          var innerY = block.y + setback;
+          var innerWidth = block.width - setback * 2;
+          var innerHeight = block.height - setback * 2;
+          if (innerWidth < plan.PLAN_CELL_SIZE ||
+              innerHeight < plan.PLAN_CELL_SIZE)
+            continue;
+
+          subdivideParcel(makeParcelRect(innerX, innerY, innerWidth, innerHeight), result, 0);
+        }
+
+      return result;
+    }
+
+// recursively split a parcel candidate into parcel rectangles
+  function subdivideParcel(parcel: ParcelRect, out: Array<ParcelRect>, depth: Int)
+    {
+      var sizingDensity = plan.getBuildingDistrictDensity(parcel.districtType);
+      var targetWidth = plan.getTargetParcelWidth(sizingDensity);
+      var targetHeight = plan.getTargetParcelHeight(sizingDensity);
+      var splitThreshold = plan.getParcelSplitThreshold(sizingDensity);
+      var canSplitX = parcel.width > targetWidth * splitThreshold;
+      var canSplitY = parcel.height > targetHeight * splitThreshold;
+      var maxDepth = plan.getParcelMaxDepth(sizingDensity);
+
+      if (depth >= maxDepth ||
+          (!canSplitX && !canSplitY))
+        {
+          out.push(finalizeParcel(parcel));
+          return;
+        }
+
+      var splitVertical = parcel.width >= parcel.height;
+      if (splitVertical && !canSplitX)
+        splitVertical = false;
+      else if (!splitVertical && !canSplitY)
+        splitVertical = true;
+
+      if (splitVertical)
+        {
+          var minSplit = Std.int(parcel.width * plan.getParcelSplitMinRatio(sizingDensity));
+          var maxSplit = Std.int(parcel.width * plan.getParcelSplitMaxRatio(sizingDensity));
+          if (maxSplit - minSplit < plan.PLAN_CELL_SIZE)
+            {
+              out.push(finalizeParcel(parcel));
+              return;
+            }
+
+          var split = parcel.x + plan.randomRangeInt(minSplit, maxSplit);
+          subdivideParcel(makeParcelRect(
+            parcel.x,
+            parcel.y,
+            split - parcel.x,
+            parcel.height), out, depth + 1);
+          subdivideParcel(makeParcelRect(
+            split,
+            parcel.y,
+            parcel.x + parcel.width - split,
+            parcel.height), out, depth + 1);
+          return;
+        }
+
+      var minSplit = Std.int(parcel.height * plan.getParcelSplitMinRatio(sizingDensity));
+      var maxSplit = Std.int(parcel.height * plan.getParcelSplitMaxRatio(sizingDensity));
+      if (maxSplit - minSplit < plan.PLAN_CELL_SIZE)
+        {
+          out.push(finalizeParcel(parcel));
+          return;
+        }
+
+      var split = parcel.y + plan.randomRangeInt(minSplit, maxSplit);
+      subdivideParcel(makeParcelRect(
+        parcel.x,
+        parcel.y,
+        parcel.width,
+        split - parcel.y), out, depth + 1);
+      subdivideParcel(makeParcelRect(
+        parcel.x,
+        split,
+        parcel.width,
+        parcel.y + parcel.height - split), out, depth + 1);
+    }
+
+// create one parcel rect with local sampled density
+  function makeParcelRect(x: Int, y: Int, width: Int, height: Int): ParcelRect
+    {
+      return {
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        density: plan.sampleAverageDensity(x, y, width, height),
+        districtType: plan.getBuildingDistrictTypeForRect(x, y, width, height),
+        isOpen: false,
+      };
+    }
+
+// finalize a parcel with plaza/open-space chance
+  function finalizeParcel(parcel: ParcelRect): ParcelRect
+    {
+      var sizingDensity = plan.getBuildingDistrictDensity(parcel.districtType);
+      var openChance = 0.0;
+      if (sizingDensity < 0.33)
+        openChance = 0.006;
+      else if (sizingDensity < 0.66)
+        openChance = 0.003;
+      else openChance = 0.010;
+
+      var centrality = plan.getParcelCentrality(parcel);
+      var area = parcel.width * parcel.height;
+      if (sizingDensity > 0.66 &&
+          parcel.width > plan.CLEAN_TILE_SIZE * 2 &&
+          parcel.height > plan.CLEAN_TILE_SIZE * 2)
+        {
+          openChance += 0.006 + centrality * 0.018;
+          if (area >= plan.CLEAN_TILE_SIZE * plan.CLEAN_TILE_SIZE * 10)
+            openChance += 0.010;
+        }
+
+      if (plan.isSkinnyParcel(parcel))
+        openChance += (sizingDensity > 0.66 ? 0.03 : 0.06);
+      if (parcel.width < plan.PLAN_CELL_SIZE * 2 ||
+          parcel.height < plan.PLAN_CELL_SIZE * 2)
+        openChance += 0.04;
+      if (sizingDensity < 0.33 &&
+          parcel.width > plan.CLEAN_TILE_SIZE * 3 &&
+          parcel.height > plan.CLEAN_TILE_SIZE * 3)
+        openChance += 0.01;
+      if (sizingDensity < 0.33 &&
+          centrality < 0.34 &&
+          area >= plan.CLEAN_TILE_SIZE * plan.CLEAN_TILE_SIZE * 4)
+        openChance += 0.01;
+
+      openChance = plan.clampFloat(openChance, 0.0, 0.92);
+
+      parcel.isOpen = plan.rng.nextFloat() < openChance;
+      return parcel;
+    }
+
+}
