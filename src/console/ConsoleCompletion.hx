ee580bc7578e6c4218eb476044314bb969b9885d
Max Kowarski <starinfidel@gmail.com>
2026-07-18 01:40:31 +0500

    feat: learn npcs debug command to fully learn some timeline npcs

--- src/console/ConsoleCompletion.hx
+++ src/console/ConsoleCompletion.hx
@@ -137,6 +137,7 @@ class ConsoleCompletion
             { lit: 'event', next: [ { slot: '[index]' } ] },
             { lit: 'improvement', next: [
               { slot: '<name>', values: function() return [ 'all' ].concat(improv()), next: [ { slot: '<level>' } ] } ] },
+            { lit: 'npcs', next: [ { slot: '[count]' } ] },
             { lit: 'region' },
             { lit: 'timeline' },
           ] });
